b49fae6b7c1903bff9032bb5d26a8331d8b60ad7
futo-develop <takumi.suzuki.works@gmail.com>
2026-04-18 07:33:13 +0900

    refactor: Simplify StatsScreen rendering in App component for improved readability

--- App.tsx
+++ App.tsx
@@ -626,12 +626,7 @@ export default function App() {
           name="統計"
           options={{ tabBarLabel: '📊 統計' }}
         >
-          {() => (
-            <StatsScreen
-              sessions={savedSessions}
-              totalGridCount={Object.keys(gridCounts).length}
-            />
-          )}
+          {() => <StatsScreen sessions={savedSessions} totalGridCount={Object.keys(gridCounts).length} />}
         </Tab.Screen>
         <Tab.Screen
           name="称号"
